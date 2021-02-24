8f84e3eac416c8278719c613efe28021ebd025f0
GluhovDmitry <you@example.com>
2021-02-24 19:47:35 +0500

    Added form validation on backend

--- food/views.py
+++ food/views.py
@@ -7,13 +7,18 @@ def home(request):
     bks = BurgerKing.objects.all()
     comp_count = 0
     competitors = []
+    adrs = ''
 #   get data from form
     if request.method == "POST":
-        adrs = request.POST.get('address')
+        form = Form(request.POST)
+        if form.is_valid():
+            adrs = form.cleaned_data['address']
 #   choose competitors in less than 2 km with func count_distance from get_distances.py
         competitors += count_distance('food_mcdonalds', adrs)
         competitors += count_distance('food_kfc', adrs)
         comp_count = len(competitors)
+    else:
+        form = Form()
 #   get address and restaurant's name
     companies = [(i[0][4], i[0][1]) for i in competitors]
-    return render(request, 'home.html', {'bks': bks, 'comp_count': comp_count, 'companies': companies, "form": Form})
+    return render(request, 'home.html', {'bks': bks, 'comp_count': comp_count, 'companies': companies, 'form': form})
