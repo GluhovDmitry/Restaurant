8a5e336aa9e98d9732a8a456bd961c42a95aa48f
GluhovDmitry <you@example.com>
2021-02-24 22:26:18 +0500

    Added css

--- food/views.py
+++ food/views.py
@@ -14,11 +14,12 @@ def home(request):
         if form.is_valid():
             adrs = form.cleaned_data['address']
 #   choose competitors in less than 2 km with func count_distance from get_distances.py
-        competitors += count_distance('food_mcdonalds', adrs)
-        competitors += count_distance('food_kfc', adrs)
-        comp_count = len(competitors)
+            competitors += count_distance('food_mcdonalds', adrs)
+            competitors += count_distance('food_kfc', adrs)
+            comp_count = len(competitors)
     else:
         form = Form()
 #   get address and restaurant's name
     companies = [(i[0][4], i[0][1]) for i in competitors]
-    return render(request, 'home.html', {'bks': bks, 'comp_count': comp_count, 'companies': companies, 'form': form})
+    return render(request, 'home.html', {'bks': bks, 'comp_count': comp_count,
+                                         'companies': companies, 'form': form, 'adrs': adrs})
